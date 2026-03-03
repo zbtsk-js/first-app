11d9d6af1215ae8c7dd2f90e3f357770644bedac
zbtsk-js <zmihail2406@gmail.com>
2026-03-03 20:53:35 +0100

    fixed the Mollie API problem and made it possible to see data fetchingw

--- front-part/src/js/components/Layout/Product/ProductCard.jsx
+++ front-part/src/js/components/Layout/Product/ProductCard.jsx
@@ -4,7 +4,6 @@ const ProductCard = (product) => {
     const {
         id,
         title,
-        subtitle,
         price,
         imageSrc,
         imageAlt,
@@ -21,28 +20,13 @@ const ProductCard = (product) => {
                     height="410"
                 />
 
-                <div className="product-card__main">
+
                     <div className="product-card__body">
                         <h3 className="product-card__title">{title}</h3>
-                        <p className="product-card__subtitle">{subtitle}</p>
+                        <p className="product-card__price">{price} NOK</p>
                     </div>
 
-                    <div className="product-card__footer">
-                        <div className="product-card__price-wrapper">
-                            <div className="product-card__price">{price} NOK</div>
-                        </div>
 
-                        <button
-                            className="product-card__buy-btn"
-                            type="button"
-                        >
-                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M8 3V13M3 8H13" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
-                            </svg>
-                            Add
-                        </button>
-                    </div>
-                </div>
             </div>
         </Link>
     );
