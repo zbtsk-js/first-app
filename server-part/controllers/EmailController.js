50c3a085e9f9df9f61fea73447aee75df5d961c1
zbtsk-js <zmihail2406@gmail.com>
2026-09-09 22:58:54 +0200

    changed methods in EMailController from function call to arrow functions so it would say "this" context in every call

--- server-part/controllers/EmailController.js
+++ server-part/controllers/EmailController.js
@@ -2,16 +2,15 @@ export class EmailController {
     constructor(UserService) {
         this.UserService = UserService;
     }
-    async checkifEmailexists(req, res, next){
+    checkifEmailexists = async(req, res, next)=> {
         try {
             const email = req.query.email
             const EmailExists = await this.UserService.checkifEmailexists(email)
             return res.json(EmailExists)
-        }catch(e){
-            next(e)        }
+        }catch(e){next(e)}
 
     }
-    async getEmailbyTheLink (req, res, next){
+     getEmailbyTheLink = async(req, res, next) =>{
         try {
             const registrationToken = req.query.registrationToken
             const Userdata = await this.UserService.getEmailbyTheLink(registrationToken)
