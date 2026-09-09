32497e2f7383a4e2562e3f8be39076801250320d
zbtsk-js <zmihail2406@gmail.com>
2026-09-09 22:54:31 +0200

    deleted _setAuthCookie because now we have a function thats imported and used generally

--- server-part/controllers/AuthController.js
+++ server-part/controllers/AuthController.js
@@ -1,22 +1,13 @@
 import {validationResult} from "express-validator";
+import {setAuthCookie} from "../utils/cookie.js";
 import dotenv from 'dotenv'
 import {ApiError} from "../exceptions/exceptions.js";
 dotenv.config()
 
+
 export class AuthController{
     constructor(UserService) {
         this.UserService = UserService;
-    }
-    _setAuthCookie = (res, refreshToken) => {
-        // 30 дней
-        const maxAge = 30 * 24 * 60 * 60 * 1000
-        res.cookie('refreshToken', refreshToken, {
-
-            httpOnly: true,
-            maxAge: maxAge,
-            sameSite: 'lax',
-            // secure: true // включите, если используете HTTPS
-        });
     }
     registration = async (req, res, next) => {
         try {
@@ -32,7 +23,7 @@ export class AuthController{
                 throw ApiError.BadRequestError('Validation error', FormattedErrors)
             }
             const UserData = await this.UserService.Registration(email, password)
-            this._setAuthCookie(res, UserData.RefreshToken)
+            setAuthCookie(res, UserData.RefreshToken,)
 
             return res.json(UserData)
         } catch (error) {
@@ -50,7 +41,7 @@ try{
         try {
             const {email, password} = req.body
             const UserData = await this.UserService.Login(email, password)
-            this._setAuthCookie(res, UserData.RefreshToken)
+            setAuthCookie(res, UserData.RefreshToken,)
             return res.json(UserData)
         } catch (error) {
             next(error)
@@ -80,7 +71,7 @@ try{
         try {
             const {registrationToken, password} = req.body
             const UserData = await this.UserService.LazyActivation(registrationToken, password)
-            this._setAuthCookie(res, UserData.RefreshToken)
+            setAuthCookie(res, UserData.RefreshToken,)
             return res.json(UserData)
         }catch(e){
 next(e)        }
@@ -89,7 +80,7 @@ next(e)        }
         try {
             const {credential} = req.body
             const UserData = await this.UserService.GoogleLogin(credential)
-            this._setAuthCookie(res, UserData.RefreshToken)
+            setAuthCookie(res, UserData.RefreshToken,)
 
             return res.json(UserData)
         }catch(e){
